c0f55cac9bfcda4882660412bce42d8ccb6aacb4
Otter <thanhduyly18@gmai.com>
2024-09-04 17:17:11 +0700

    prisma

--- api/controler/auth.controler.js
+++ api/controler/auth.controler.js
@@ -1,17 +1,54 @@
+import prisma from "../lib/prisma.js";
 import bcryct from "bcrypt"; //tạo mã hash
 
 export const register = async (req, res) => {
   const { username, password, email } = req.body;
   console.log(req.body);
 
-  //HASH PASSWORD
-  const hasedPassword = await bcryct.hash(password, 10);
+  try {
+    //HASH PASSWORD
+    const hashedPassword = await bcryct.hash(password, 10);
 
-  //NEW USER
+    //CREATE A NEW USER AND SAVE TO DB
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    console.log(newUser);
+
+    res.status(201).json({ message: "User created successfully!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to create user" });
+  }
 };
 
-export const login = (req, res) => {
-  console.log(req.body);
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    //CHECK IF THE USER EXISTS
+
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) return res.status(401).json({ message: "Invalid Credentials!" });
+
+    //CHECK IF THE PASSWORD IS CORRECT
+    const isPasswordValid = await bcryct.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid Credentials!" });
+
+    //GENERATE COCKIE TOKEN AND SEND TO THE USER
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to login!" });
+  }
 };
 
 export const logout = (req, res) => {
