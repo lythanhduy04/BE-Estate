b35df8ef320aa2d27a771d07e0db0bacd3b184d5
Otter <thanhduyly18@gmai.com>
2024-09-11 16:10:18 +0700

    middleware

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.router.js";
 
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));//cấp chứng chỉ để truy cập api
 app.use(express.json()); //cho phép truyền file json
 app.use(cookieParser()); //đọc cookie dễ
 
